3e669c7bf7dd3a94de38022acac7ff17ddb5255c
Ankur <am8468616@gmail.com>
2025-05-10 18:49:17 +0530

    optimized the codebase for testing the environment and dependencies

---
+++ backend/controller/authController.js
@@ -0,0 +1,10 @@
+export function login(req, res) {
+    res.send('Login Page');
+}
+export function signup(req, res) {
+    res.send('Signup Page');
+}   
+export function logout(req, res) {
+    res.send('Logout Page');
+}
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "../routes/authRoutes.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.get("/api/auth/signup", (req, res) => {
-  res.send("/api/auth/Signup");
-});
-app.get("/api/auth/login", (req, res) => {
-  res.send("/api/auth/Login");
-});
-app.get("/api/auth/logout", (req, res) => {
-  res.send("Logout");
-});
+
+app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
