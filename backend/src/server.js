7e118db32ade48c8ed6409653ae89dfeaddb1108
Ankur <am8468616@gmail.com>
2025-05-11 06:39:46 +0530

    feat(backend): Set up MongoDB connection and refactor file structure

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "../routes/authRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import connectDB from "./lib/db.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
