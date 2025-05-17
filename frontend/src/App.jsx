8650af32a4ea207b33d900fcf3ea39c60ee21291
Ankur <am8468616@gmail.com>
2025-05-17 10:41:59 +0530

    frontend dependency and design ui testing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen ' data-theme="retro">
+      App
+     <button className="btn btn-outline btn-info">Info</button>
+<button className="btn btn-outline btn-success">Success</button>
+<button className="btn btn-outline btn-warning">Warning</button>
+<button className="btn btn-outline btn-error">Error</button>
+    </div>
+  )
+}
+
+export default App
