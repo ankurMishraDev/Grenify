1ce84b9a7e549866a7d149816ca47f384d8b4964
Ankur <am8468616@gmail.com>
2025-05-18 18:15:30 +0530

    signup page design under process

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,64 @@
-import { Routes, Route } from 'react-router';
-import CallPage  from './pages/CallPage.jsx';
-import  HomePage  from './pages/HomePage.jsx';
-import  LoginPage  from './pages/LoginPage.jsx';
-import  NotificationPage  from './pages/NotificationPage.jsx';
-import  OnboardingPage  from './pages/OnboardingPage.jsx';
-import  SignUpPage  from './pages/SignUpPage.jsx';
-import  ChatPage  from './pages/ChatPage.jsx';
-import  { Toaster}  from 'react-hot-toast';
-import { useQuery } from '@tanstack/react-query';
-import { axiosInstance } from './lib/axios.js';
+import { Routes, Route, Navigate } from "react-router";
+import CallPage from "./pages/CallPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import NotificationPage from "./pages/NotificationPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios.js";
 const App = () => {
-  const {data, isLoading, error}= useQuery({
-    queryKey: ["todos"],
-    queryFn: async ()=>{
+  const {
+    data: authData,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
       const res = await axiosInstance.get("/auth/me");
       return res.data;
-    }, 
-    retry: false,// Disable retry
-  })
-  console.log(data);
-  
+    },
+    retry: false, // Disable retry
+  });
+  const authUser = authData?.user;
 
   return (
-    <div className='h-screen ' data-theme="black">
-      
-     <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/call' element={<CallPage />} />
-      <Route path='/chat' element={<ChatPage />} />
-      <Route path='/notification' element={<NotificationPage />} />
-      <Route path='/onboarding' element={<OnboardingPage />} />
-      <Route path='/signup' element={<SignUpPage />} />
-     </Routes>
-     <Toaster />
+    <div className="h-screen " data-theme="black">
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/call"
+          element={authUser ? <CallPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/chat"
+          element={authUser ? <ChatPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/notification"
+          element={authUser ? <NotificationPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/onboarding"
+          element={authUser ? <OnboardingPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authData ? <SignUpPage /> : <Navigate to="/" />}
+        />
+      </Routes>
+      <Toaster />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
-
+import { useState } from "react";
+import { MessageSquareText } from "lucide-react";
 const SignUpPage = () => {
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+  const handleSignup = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-    <h1>Sign up page</h1>  
+    <div
+      className="flex h-screen items-center justify-center p-4 sm:p-6 md:p-8"
+      data-theme="luxury"
+    >
+      <div
+        className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl 
+        mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden"
+      >
+        {/* Left side*/}
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+          {/*Logo  */}
+          <div className="mb-4 flex items-center justify-start gap-2">
+            <MessageSquareText className="size-9 text-primary" />
+            <span
+              className="text-3xl font-bold font-mono bg-clip-text text-transparent 
+                bg-gradient-to-r from-primary to-secondary tracking-wider"
+            >
+              Grenify
+            </span>
+          </div>
+          {/* Form */}
+          <div className="w-full">
+            <form onSubmit={handleSignup}>
+              <div className="space-y-4">
+                <div>
+              <h2 className="text-xl font-semibold">Create an Account</h2>
+              < p className="text-sm opacity-80"> Join Grenify and start your adventure! </p>
+              </div>
+              <div className="space-y-3">
+                <div className='form-control w-full'>
+                    <label className="label"></label>
+                </div>
+              </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignUpPage
+export default SignUpPage;
