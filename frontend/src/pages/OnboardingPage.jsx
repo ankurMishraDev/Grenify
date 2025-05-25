ca46b6aa197ef6df4d2299505c384516b19fb028
Ankur <am8468616@gmail.com>
2025-05-25 16:12:03 +0530

    onboarding page design under process, final test pending

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,11 +1,201 @@
-import React from 'react'
-
+import { useState } from "react";
+import useAuthUser from "../hooks/useAuthUser";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { completeOnboarding } from "../lib/api";
+import { toast } from "react-hot-toast";
+import { CameraIcon, MapPinIcon } from "lucide-react";
+import { LANGUAGES } from "../constants";
 const OnboardingPage = () => {
-  return (
-    <div>
-     <h1>Onboarding page</h1> 
-    </div>
-  )
-}
+  const { authUser } = useAuthUser();
+  const queryClient = useQueryClient();
+  const [formState, setFormState] = useState({
+    fullName: authUser?.fullName || "",
+    bio: authUser?.bio || "",
+    nativeLanguage: authUser?.nativeLanguage || "",
+    learningLanguage: authUser?.learningLanguage || "",
+    location: authUser?.location || "",
+    profilePic: authUser?.profilePic || "",
+  });
 
-export default OnboardingPage
+  const { mutate, isPending } = useMutation({
+    mutationFn: completeOnboarding,
+    onSuccess: () => {
+      toast.success("Onboarding completed successfully");
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    },
+    onError: (error) => {
+      console.error("Error completing onboarding:", error);
+      toast.error(error.response.data.messageFields);
+    },
+  });
+  const handleAvatarChange = () => {
+    const idx = Math.floor(Math.random() * 100) + 1;
+    const newAvatarPic = `https://avatar.iran.liara.run/public/${idx}.png`;
+    setFormState({ ...formState, profilePic: newAvatarPic });
+    toast.success("Avatar picture updated successfully");
+    
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    mutate(formState);
+  };
+  return (
+    <div className="min-h-screen bg-base-100 flex items-center justify-center p-2">
+      <div className="card bg-base-200 w-full max-w-3xl shadow-xl  max-h-[80vh]">
+        <div className="card-body p-6 sm:p-8 overflow-y-auto">
+          <h1 className="text-2xl sm:text-3xl font-bold text-center mb-4">
+            Kindly Fill Details
+          </h1>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            {/*profile pic*/}
+            <div className="flex flex-col items-center justify-center space-y-4">
+              {/*image preview*/}
+              <div className="size-32 rounded-full bg-base-300 overflow-hidden">
+                {formState.profilePic ? (
+                  <img
+                    src={formState.profilePic}
+                    alt="Profile image preview"
+                    className="w-full h-full object-cover"
+                  />
+                ) : (
+                  <div className="flex items-center justify-center h-full">
+                    <CameraIcon className="size-12 text-base-content opacity-50" />
+                  </div>
+                )}
+              </div>
+              {/* change avatar pic button */}
+              <div className="flex items-center gap-1">
+                <button
+                  className="btn btn-accent"
+                  onClick={handleAvatarChange}
+                  type="button"
+                >
+                  Change Avatar Pic
+                </button>
+              </div>
+            </div>
+
+            {/* Full Name */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Full Name</span>
+              </label>
+              <input
+                type="text"
+                name="fullName"
+                value={formState.fullName}
+                onChange={(e) =>
+                  setFormState({ ...formState, fullName: e.target.value })
+                }
+                className="input input-bordered w-full"
+                placeholder="Enter your full name"
+                required
+              />
+            </div>
+
+            {/* Bio */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Bio</span>
+              </label>
+              <input
+                type="text"
+                name="bio"
+                value={formState.bio}
+                onChange={(e) =>
+                  setFormState({ ...formState, bio: e.target.value })
+                }
+                className="input input-bordered w-full"
+                placeholder="Enter your Bio details here"
+                required
+              />
+            </div>
+
+            {/* Language */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+              {/* Native Language */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Native Language</span>
+                </label>
+                <select
+                  name="nativeLanguage"
+                  value={formState.nativeLanguage}
+                  onChange={(e) =>
+                    setFormState({
+                      ...formState,
+                      nativeLanguage: e.target.value,
+                    })
+                  }
+                  className="select select-bordered w-full"
+                >
+                  <option value="">Select your native language</option>
+                  {LANGUAGES.map((lang) => (
+                    <option key={lang} value={lang.toLowerCase()}>
+                      {lang}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              {/* Learning Language */}
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Learning Language</span>
+                </label>
+                <select
+                  name="learningLanguage"
+                  value={formState.learningLanguage}
+                  onChange={(e) =>
+                    setFormState({
+                      ...formState,
+                      learningLanguage: e.target.value,
+                    })
+                  }
+                  className="select select-bordered w-full"
+                >
+                  <option value="">Select language you're learning</option>
+                  {LANGUAGES.map((lang) => (
+                    <option key={lang} value={lang.toLowerCase()}>
+                      {lang}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            {/* Location */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Location</span>
+              </label>
+              <div className="relative">
+                <MapPinIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 size-5 text-base-content opcacity-50" />
+                <input
+                  type="text"
+                  name="location"
+                  value={formState.location}
+                  onChange={(e) =>
+                    setFormState({ ...formState, location: e.target.value })
+                  }
+                  className="input input-bordered w-full pl-10"
+                  placeholder="Enter your location: City, Country"
+                />
+              </div>
+            </div>
+
+            {/* Submit Button */}
+            <button
+              className="btn btn-primary w-full"
+              disabled={isPending}
+              type="submit"
+            >
+              {!isPending ? <>Provide all Details</> : <>Updating Details</>}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OnboardingPage;
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -11,41 +11,41 @@ const SignUpPage = () => {
   });
 
   const queryClient = useQueryClient();
-  const {mutate, isPending, error} = useMutation({
+  const { mutate, isPending, error } = useMutation({
     mutationFn: signup,
-    onSuccess: () =>queryClient.invalidateQueries({queryKey:["authUser"]}),
-  })
-   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  });
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const images = [
     {
       src: "/signupImage4.png",
       title: "Connect with people from anywhere",
-      description: "Interact with people and grow together"
+      description: "Interact with people and grow together",
     },
     {
-      src: "/signupImage1.png", 
+      src: "/signupImage1.png",
       title: "Build meaningful relationships",
-      description: "Create lasting connections that matter"
+      description: "Create lasting connections that matter",
     },
     {
       src: "/signupImage3.png",
-      title: "Share experiences together", 
-      description: "Discover new perspectives and ideas"
+      title: "Share experiences together",
+      description: "Discover new perspectives and ideas",
     },
     {
       src: "/signupImage2.svg",
       title: "Join a vibrant community",
-      description: "Be part of something bigger than yourself"
-    }
+      description: "Be part of something bigger than yourself",
+    },
   ];
   const handleSignup = (e) => {
     e.preventDefault();
     mutate(signupData);
   };
-    useEffect(() => {
+  useEffect(() => {
     // Change image every 4 seconds (4000ms) - ADJUST INTERVAL HERE
     const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => 
+      setCurrentImageIndex((prevIndex) =>
         prevIndex === images.length - 1 ? 0 : prevIndex + 1
       );
     }, 7000);
@@ -78,11 +78,11 @@ const SignUpPage = () => {
             </span>
           </div>
           {/* errororor message */}
-        {error &&(
-          <div className="alert alert-erroror mb-4">
-            <span>{error.response.data.message}</span>
-          </div>
-        )}
+          {error && (
+            <div className="alert alert-erroror mb-4">
+              <span>{error.response.data.message}</span>
+            </div>
+          )}
           {/* Form */}
           <div className="w-full">
             <form onSubmit={handleSignup}>
@@ -104,13 +104,18 @@ const SignUpPage = () => {
                       placeholder="Greninja"
                       className="input input-bordered w-full"
                       value={signupData.fullName}
-                      onChange={(e)=>setSignupData({...signupData, fullName: e.target.value})}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          fullName: e.target.value,
+                        })
+                      }
                       required
                     />
                   </div>
                 </div>
                 {/* Email */}
-                 <div className="space-y-3">
+                <div className="space-y-3">
                   <div className="form-control w-full">
                     <label className="label">
                       <span className="label-text">Email:</span>
@@ -120,13 +125,15 @@ const SignUpPage = () => {
                       placeholder="gre@xyz.com"
                       className="input input-bordered w-full"
                       value={signupData.email}
-                      onChange={(e)=>setSignupData({...signupData, email: e.target.value})}
+                      onChange={(e) =>
+                        setSignupData({ ...signupData, email: e.target.value })
+                      }
                       required
                     />
                   </div>
                 </div>
                 {/* Password */}
-                 <div className="space-y-3">
+                <div className="space-y-3">
                   <div className="form-control w-full">
                     <label className="label">
                       <span className="label-text">Password:</span>
@@ -136,56 +143,74 @@ const SignUpPage = () => {
                       placeholder="********"
                       className="input input-bordered w-full"
                       value={signupData.password}
-                      onChange={(e)=>setSignupData({...signupData, password: e.target.value})}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          password: e.target.value,
+                        })
+                      }
                       required
                     />
                     <p className="text-xs opacity-70 mt-1">
-                      Password must be at least 6 characters long and contain a mix of letters, numbers, and symbols.
+                      Password must be at least 6 characters long and contain a
+                      mix of letters, numbers, and symbols.
                     </p>
                   </div>
                 </div>
                 {/* Submit Button */}
-                <button className="btn btn-primary w-full" type="submit">{isPending?(<><span className="loading loading-spinner loading-xs"></span> Wait a second..</>):("Create Account")}</button>
+                <button className="btn btn-primary w-full" type="submit">
+                  {isPending ? (
+                    <>
+                      <span className="loading loading-spinner loading-xs"></span>{" "}
+                      Wait a second..
+                    </>
+                  ) : (
+                    "Create Account"
+                  )}
+                </button>
                 <div className="text-center mt-4">
-                  <p className="text-sm">Already have an account?{" "}<Link to="/login" className="text-primary hover:underline">Sign in</Link></p>
+                  <p className="text-sm">
+                    Already have an account?{" "}
+                    <Link to="/login" className="text-primary hover:underline">
+                      Sign in
+                    </Link>
+                  </p>
                 </div>
-                
               </div>
             </form>
           </div>
         </div>
 
         {/* Right side */}
-       <div className="hidden lg:flex w-full lg:w-1/2 bg-primary/10 items-center justify-center">
-      <div className="max-w-md p-8">
-        {/* Image with smooth transition */}
-        <div className="relative aspect-square w-full max-w-[36.8rem] mx-auto overflow-hidden rounded-lg bg-base-100">
-          {images.map((image, index) => (
-    <img
-      key={index}
-      src={image.src}
-      alt={`Illustration ${index + 1}`}
-      className={`absolute top-0 left-0 w-full h-full object-cover transform-gpu transition-opacity duration-1000 ease-in-out  ${
-        index === currentImageIndex 
-          ? "opacity-100 z-10" 
-          : "opacity-0 z-0"
-      }`}
-    />
-  ))}
+        <div className="hidden lg:flex w-full lg:w-1/2 bg-primary/10 items-center justify-center">
+          <div className="max-w-md p-8">
+            {/* Image with smooth transition */}
+            <div className="relative aspect-square w-full max-w-[36.8rem] mx-auto overflow-hidden rounded-lg bg-base-100">
+              {images.map((image, index) => (
+                <img
+                  key={index}
+                  src={image.src}
+                  alt={`Illustration ${index + 1}`}
+                  className={`absolute top-0 left-0 w-full h-full object-cover transform-gpu transition-opacity duration-1000 ease-in-out  ${
+                    index === currentImageIndex
+                      ? "opacity-100 z-10"
+                      : "opacity-0 z-0"
+                  }`}
+                />
+              ))}
+            </div>
+
+            {/* Dynamic text content */}
+            <div className="text-center space-y-3 mt-6">
+              <h2 className="text-xl font-semibold transition-opacity duration-500">
+                {currentImage.title}
+              </h2>
+              <p className="opacity-70 transition-opacity duration-500">
+                {currentImage.description}
+              </p>
+            </div>
+          </div>
         </div>
-        
-        {/* Dynamic text content */}
-        <div className="text-center space-y-3 mt-6">
-          <h2 className="text-xl font-semibold transition-opacity duration-500">
-            {currentImage.title}
-          </h2>
-          <p className="opacity-70 transition-opacity duration-500">
-            {currentImage.description}
-          </p>
-        </div>
-      </div>
-    </div>
-        
       </div>
     </div>
   );
